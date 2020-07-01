d0d206c45330454160b8db80e5d0d4254622834d
SwingGuy1024 <SwingGuy1024@yahoo.com>
2020-07-01 13:19:32 -0700

    Initial Release

---
+++ src/site/js/vulcan.js
@@ -0,0 +1,24 @@
+function leadText() {
+  return '  <h1>Vulcan&rsquo;s Level</h1>\n' +
+      '  <p class="lead">\n' +
+      '  Vulcan, Roman god of the forge, the only god who worked, was known for the beautiful things he crafted in his foundry. Vulcan used only\n' +
+      '  the best tools. This carpenters level is the level that Vulcan would have used. If he existed. And if he had a smart phone.</p>\n' +
+      '<p>\n' +
+      '  Okay, maybe a Roman god wouldn&rsquo;t have used a level app on a smart phone. Maybe he could just put his hands on a surface and feel even the\n' +
+      '  smallest tilt. I mean, he was a god, right? So what makes this level app so useful that it bears the name of a Roman god?\n' +
+      '</p><p>\n' +
+      '  It uses a unique calibration process that doesn&rsquo;t require a level surface.\n' +
+      '</p><p>\n' +
+      '  Yeah, there are a lot of carpenter&rsquo;s level apps out there. And while most smart phones have a gravity sensor, most of them aren&rsquo;t\n' +
+      '  installed with precision, so most of these apps aren&rsquo;t very accurate unless you can calibrate them. A lot of smart-phone levels have a\n' +
+      '  &ldquo;Calibrate&rdquo; button, but you first need to put your device on a surface that you already know is level. And to know that, you need a\n' +
+      '  carpenter&rsquo;s level that&rsquo;s already calibrated!\n' +
+      '</p><p>\n' +
+      '  So how useful is that? Well, not very. So your level&rsquo;s calibration button probably doesn&rsquo;t do you much good.\n' +
+      '</p><p>\n' +
+      '  Here&rsquo;s where Vulcan&rsquo;s Level beats all the other smart-phone levels: It uses a unique two-stage calibration process. By measuring a surface\n' +
+      '  from two different orientations, it can compensate for a tilted surface. So it can give you a true calibration, even from a surface that&rsquo;s\n' +
+      '  not truly level. Kind of like putting your hands on a surface and just feeling even the smallest tilt. How does that work? Well, download\n' +
+      '  the app and try it yourself.\n' +
+      '</p>\n';
+}
